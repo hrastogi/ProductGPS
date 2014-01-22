2175d91fbeaf6c450b21034d519f5e050efd68a5
Heena Rastogi <rastogi.heena@gmail.com>
2014-01-21 23:43:37 -0800

    added product detail

---
+++ ProductGPS/ProductGPS/Application/UI/ItemDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  ItemDetailViewController.m
+//  ProductGPS
+//
+//  Created by HEENA RASTOGI on 1/21/14.
+//  Copyright (c) 2014 Heena. All rights reserved.
+//
+
+#import "ItemDetailViewController.h"
+
+@interface ItemDetailViewController ()
+
+@end
+
+@implementation ItemDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
